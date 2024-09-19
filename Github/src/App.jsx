a531e9daa1b6c7f4ae6f9171b67de1b2940e88db
Jidnesh Meher <meherjidnesh89@gmail.com>
2024-09-20 02:24:05 +0530

    Add changes

--- Github/src/App.jsx
+++ Github/src/App.jsx
@@ -4,10 +4,7 @@ function App() {
 
   return (
     <>
-      <h1>Hello Jidnesh</h1>
-      <h1>Hello Ganesh !!!</h1>
-      <h2>Good Afternoon by Jidnesh</h2>
-      <h2>Good Afternoon by Jidnesh Again</h2>
+      <h1>Testing by Raistar</h1>
     </>
   )
 }
